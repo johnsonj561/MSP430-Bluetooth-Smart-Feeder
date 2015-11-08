70d5fcf2733830a64626632c6e8e38e29256ccc3
Justin Johnson <johnsonj561@gmail.com>
2015-11-07 19:10:06 -0500

    Added IR Sensor to detect animals eating for purpose of stopping motor activity while animal is present, for safety.

--- main.c
+++ main.c
@@ -1,8 +1,8 @@
 /*Justin Johnson - FAU 2015
- *MSP430 communication with BlueSmirf Bluetooth Module
+ *MSP430 communication with BlueSmirf Bluetooth Module V5
  *Micro remains in low power mode until interrupt is triggered by incoming data
  *Commands: W = measures weight (W) sensor and sends value to bluetooth module for to provide user feedback
- *Commands: F = flashes LED P1.6, motor to be added to dispense food (F) to dog bowl
+ *Commands: F = activates servo motor on P1.6
  *baudrate = 9600
  *TXD = P1.1
  *RXD = P1.2
@@ -10,15 +10,26 @@
  */
 #include <msp430g2253.h>
 
+#define MCU_CLOCK			1000000
+#define PWM_FREQUENCY		50
+
+unsigned int PWM_Period		= (MCU_CLOCK / PWM_FREQUENCY);	// PWM Period
+unsigned int PWM_Duty		= 500;							// %
 unsigned char Rx_Data = 0;					//temp RX variable
 unsigned char Tx_Data = 0;					//temp TX variable
 int weight = 0;								//stores weight of food supply
-int count = 0;								//Timer A
-int motorActive = 0;
+int ir = 0;									//stores value of IR sensor
+int ADCReading [2];							//array to store analog digital conversion values
+int motorActive = 0;						//flag to control motor activity
+int i;										//counter
 
-void sendFoodSupply(int);
-void UARTSendArray(char *TxArray, int ArrayLength);
-void clearUART(void);
+int stopMotor(void);						//prevents motor operation if object is detected within 2 ft proximity
+void dispenseFood(void);					//activates servo motor to dispense food
+void sendFoodSupply(int);					//sends appropriate message to mobile device via Bluetooth
+void UARTSendArray(char *TxArray, int ArrayLength);//sends message through Bluetooth Module
+void clearUART(void);						//clear UART buffer to ensure all data has been sent/received
+void configureADC(void);					//configure analog digital convertor - P1.4 (weight sensor) P1.5 (IR sensor)
+void readADC(void);							//read values from weight sensor and IR sensor
 
 int main(void)
 {
@@ -35,9 +46,15 @@ int main(void)
   /*** Set-up GPIO ***/
   P1SEL = BIT1 + BIT2;					// P1.1 = TXD, P1.2=RXD
   P1SEL2 = BIT1 + BIT2;
-  P1DIR |= (~BIT0); 					// set bit 4 as input
-  P1DIR |= BIT6;						// P1.6 set as output
-  P1OUT &= ~(BIT6);						// P1.6 OFF
+  P1DIR |= (~BIT0) + (~BIT3) + BIT6; 	// P1.4 (weight sensor) P1.5 (ir sensor) P1.6 (servo motor)
+  P1DIR |= BIT6;						// P1.6 output PWM to servo motor
+  P1SEL |= BIT6;
+
+  /*** Set-up PWM for Servo ***/
+  TACCTL1	= OUTMOD_7;            // TACCR1 reset/set
+  TACTL	= TASSEL_2 + MC_1;     	   // SMCLK, upmode
+  TACCR0	= PWM_Period-1;        // PWM Period
+  TACCR1	= PWM_Duty;            // TACCR1 PWM Duty Cycle
 
   /*** Set-up USCI A ***/
   UCA0CTL1 |= UCSSEL_2;					// SMCLK
@@ -48,41 +65,30 @@ int main(void)
   IE2 |= UCA0RXIE;						// Enable USCI_A0 RX interrupt
   __bis_SR_register(LPM0_bits + GIE);	// Enter LPM0 with interrupts enabled
 
+  /*** Set-up ADC Conversion ***/
+  configureADC();
+
   while(1){
     switch (Rx_Data){
-      case 0x57:						//If Weight Command 'W'
-      TA0CCTL0 &= ~CCIE;				//Disable Timer0_A interrupts
-      __delay_cycles(200);
-      int i = 0;
-      for(i=1; i<=10 ; i++){			//sample weight 10 times
-        ADC10CTL1 |= CONSEQ1;		// Set conversion to single channel and continuous-sampling mode
-        ADC10CTL0 |= ADC10SHT_2 + ADC10ON + MSC;   //Set S/H time, 10-bit converter, and continuous sampling:
-        ADC10AE0 |= 1;				//Choose P1.0 (channel 1) as an analog input pin:
-        ADC10CTL0 |= ADC10SC + ENC;// Start A/D conversion; The result will appear in the memory variable "ADC10MEM"
-        weight += ADC10MEM;		//store weight sensor reading
-      }
-      weight = weight/10;				//average weight
-      sendFoodSupply(weight);			//send weight value to TX Buffer
-      clearUART();						//clear TX Buffer
-      break;
+    	//Weight Command 'W' = 0x57; Receives signal from pressure sensor and transmits data to BlueSmirf//
+    	case 0x57:
+    		readADC();						//read analog values from sensors
+    		sendFoodSupply(weight);			//send weight value to TX Buffer
+    		clearUART();						//clear TX Buffer
+		break;
 
-      case 0x46:					//If Feed Command 'F', Rotate Motor
-    	  motorActive = !motorActive;
-    	  if(motorActive){
-    	      /*** Timer0_A Set-Up ***/		//FLASHING LEDS symbolizing motor rotation
-    	      TA0CCR0 |= 10000-1;			// Counter value
-    	      TA0CCTL0 |= CCIE;				// Enable Timer0_A interrupts
-    	      TA0CTL |= TASSEL_2 + MC_1;	// ACLK, Up Mode (Counts to TA0CCR0)
-    	      /*** Timer0_A Set-Up ***/
-    	  }
-    	  else{
-    	      TA0CCTL0 &= ~CCIE;				//Disable Timer0_A interrupts
-    		  P1OUT &= ~(BIT6);				// P1.6 OFF
-    	  }
-      break;
-
+		//Feed Command 'F' = 0x46; Activates servo to dispense food from hopper to bowl//
+    	case 0x46:
+    		if(!stopMotor()){				//check IR sensor to make sure it's safet to operate motor
+    			if(!motorActive){
+    				motorActive = 1;		//motorActive flag prevents user from activating motor repeatedly
+    				dispenseFood();			//activate motor
+    			}
+    			__delay_cycles(3000000);	//delay forces motor to return to 0 and properly refill dispenser with food
+    			motorActive = 0;			//reset flag
+    		}
+		break;
       //additional commands to be added at later time
-
       default: break;
     }
     __bis_SR_register(LPM0_bits);		// Enter LPM0, interrupts enabled
@@ -90,37 +96,61 @@ int main(void)
 }
 
 /**
+ * Set PWM values to rotate servo motor 180 degrees, delay, then return to original position
+ */
+void dispenseFood(){
+	//rotate 180deg and delay
+	TACCR1 = 2500;
+	__delay_cycles(3000000);
+	//rotate back to 0deg
+	TACCR1 = 500;
+}
+
+/**
+ * Check IR sensors and return true if object is present within 2 feet of dog bowl
+ */
+int stopMotor(){
+	readADC();					//read analog values from sensors
+	if(ir > 200){				//if IR detects object in close proximity, stop motor for safety
+		return 1;
+	}
+	else{						//else - allow motor operation, no object detected
+		return 0;
+	}
+}
+
+/**
  * Send appropriate message to Bluetooth device
  */
 void sendFoodSupply(int weight){
-  if(weight < 5){
+  if(weight < 10){
     UARTSendArray("Empty Bowl", 10);
   }
-  else if(weight < 8){
+  else if(weight < 20){
     UARTSendArray("10 %", 4);
   }
-  else if(weight < 12){
+  else if(weight < 30){
     UARTSendArray("20 %", 4);
   }
-  else if(weight < 15){
+  else if(weight < 40){
     UARTSendArray("30 %", 4);
   }
-  else if(weight < 18){
+  else if(weight < 50){
     UARTSendArray("40 %", 4);
   }
-  else if(weight < 22){
+  else if(weight < 60){
     UARTSendArray("50 %", 4);
   }
-  else if(weight < 26){
+  else if(weight < 70){
     UARTSendArray("60 %", 4);
   }
-  else if(weight < 29){
+  else if(weight < 80){
     UARTSendArray("70 %", 4);
   }
-  else if(weight < 32){
+  else if(weight < 90){
     UARTSendArray("80 %", 4);
   }
-  else if(weight < 35){
+  else if(weight < 100){
     UARTSendArray("90 %", 4);
   }
   else{
@@ -128,6 +158,31 @@ void sendFoodSupply(int weight){
   }
 }
 
+void configureADC(){
+   ADC10CTL1 = INCH_5 | CONSEQ_1; 				//A5 and A4, single sequence
+   ADC10CTL0 = ADC10SHT_2 | MSC | ADC10ON;
+   while (ADC10CTL1 & BUSY);
+   ADC10DTC1 = 0x02; 							// 2 conversions - this prevents conversions on A3 A2 and A1
+   ADC10AE0 |= (BIT4 + BIT5); 					//Disables digital input on P1.4 and P1.5
+}
+
+void readADC(){
+	weight = 0;
+	ir = 0;
+	for(i=1; i<=10 ; i++){
+				ADC10CTL0 &= ~ENC;					//ADC10 Enable Conversion
+				while (ADC10CTL1 & BUSY);			//Wait while ADC is busy
+				ADC10SA = (unsigned)&ADCReading[0]; //RAM Address of ADC Data, must be reset every conversion
+				ADC10CTL0 |= (ENC | ADC10SC); 		//Start ADC Conversion
+				while (ADC10CTL1 & BUSY); 			//Wait while ADC is busy
+
+				ir += ADCReading[0];
+				weight += ADCReading[1];
+			}
+			ir = ir/10;
+			weight = weight/10;
+
+}
 /**
  * Clear TX Buffer and push any pending data to Bluetooth module
  */
@@ -155,7 +210,6 @@ __interrupt void USCI0RX_ISR(void){
   __bic_SR_register_on_exit(LPM0_bits);//Wake-up CPU
 }
 
-
 //TX Interrupt. Use enable/disable to call/cancel interrupt
 //UC0IE |= UCA0TXIE;			//Enable USCI_AO TX interrupt
 //UC0IE &= ~UCA0TXIE; 			//Disable USCI_A0 TX interrupt
@@ -165,15 +219,4 @@ __interrupt void USCI0TX_ISR(void)
   UCA0TXBUF = Tx_Data;		//send data
   Tx_Data = 0;				//clear data
   UC0IE &= ~UCA0TXIE; 		//Disable USCI_A0 TX interrupt
-}
-
-
-//Timer0 A0 interrupt service routine
-#pragma vector=TIMER0_A0_VECTOR
-__interrupt void Timer0_A0 (void) {
-	count++;
-	if (count == 20){
-		P1OUT ^= BIT6;					//Toggle Green LEDs
-		count =0;
-	}
 }
